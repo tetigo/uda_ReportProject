b7b5b76ea2a30f8e9427936903fa73f851b7033b
Tiago Mendes <tiago.mendes@tbfg.com.br>
2018-10-16 17:13:34 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,51 @@
+
+# Site Info Internal Report Project
+A class to get dynamic report about Articles, Authors, Logs in a ficticious site.
+This project is about trainning your python db-api and sql skills against a database with over 1.5 million rows.
+
+### Prerequisites
+You must have Python and PostgreSQL installed. 
+Tested with Python versions 2.7.12 and 3.6.6.
+Tested with PostgreSQL version 9.5.14.
+
+### Setup
+First of all you must have the database installed on your machine.
+To create the database and load the data do the following:
+1. Enter the database command line: `psql`
+2. Create a database named 'news': `create database news;`
+3. Load data to the database: `psql -d news -f newsdata.sql;`
+
+Just that!
+Now the database contains 3 tables:
+- authors: Info about articles authors
+- articles: The articles
+- log: Every time that a user access the site, this table gets an input
+
+Now you are ready to play with the database and tables inside it using SQL or go to the next step where you are going to play with Python.
+
+### Usage
+Create an instance of Report class and call the methods:
+- get_top3_articles(): to get top 3 most visited articles
+- get_top3_authors(): to get top 3 authors from top 3 articles
+- get_percent1_error(): to get date where errors > 1% from all views
+
+### Example
+```
+report = Report()
+report.get_top3_articles()
+report.get_top3_authors()
+report.get_percent1_error()
+```
+
+### Important Notes
+I decided to use decorators for ease of command line printing and the possibility of turn them off easily and fast by just commenting the line to be able to reuse the real methods in another purpose.
+I've created 2 views(success and failure) to facilitate the answer of get_percent1_error() method, but both are included in the main query of the method so you do not have to worry about it. =))
+But if you want, you can extract the views out of the query and install them on the database on your own.
+
+#### Author
+Tiago Mendes
+<tetigo@gmail.com>
+
+If you wanna help, let me know!
+Have fun! =))
+
---
+++ report.py
@@ -0,0 +1,174 @@
+import psycopg2
+
+
+def header(text):
+    character = '-'
+    size_text = len(text)
+    size_total = 60
+    left = (size_total - size_text) / 2
+    right = left
+    if(right + left + size_text < size_total):
+        right += size_total - (right + left + size_text)
+    print(character * left + text + character * right)
+
+
+def footer():
+    print('-' * 60 + '\n')
+
+
+def calc_tabs(text):
+    tabs = ''
+    size = len(text) / 8
+    if(size >= 4):
+        return '\t'
+    elif(4 > size >= 3):
+        return '\t\t'
+    elif(3 > size >= 2):
+        return '\t\t\t'
+    elif(2 > size >= 1):
+        return '\t\t\t\t'
+    else:
+        return '\t\t\t\t\t'
+
+
+def body(self, col0, col1, function):
+    print(col0 + calc_tabs(col0) + col1)
+    for each in function(self):
+        print(str(each[0]) + calc_tabs(str(each[0])) + each[1])
+
+
+def decorate_top3_articles(function):
+    '''
+        Decorator just to display the articles in a cool format on the screen
+    '''
+    def wrap(self):
+        header('Articles Info')
+        body(self, 'Top 3 Articles Most Accessed', 'Views', function)
+        footer()
+    return wrap
+
+
+def decorate_top3_authors(function):
+    '''
+        Decorator just to display the authors in a cool format on the screen
+    '''
+    def wrap(self):
+        header('Authors Info')
+        body(self, 'Name', 'Views', function)
+        footer()
+    return wrap
+
+
+def decorate_percent1_error(function):
+    '''
+        Decorator just to display the percent in a cool format on the screen
+    '''
+    def wrap(self):
+        header('Errors Info')
+        body(self, 'Date', 'Percent', function)
+        footer()
+    return wrap
+
+
+class Report(object):
+    '''
+        A class to get dynamic report about Articles, Authors, Logs
+        in a ficticious site.
+        Usage:
+        Create an instance of Report class and call the methods:
+        - get_top3_articles(): get top 3 most visited articles
+        - get_top3_authors(): get top 3 authors from top 3 articles
+        - get_percent1_error(percent): get date where errors > percent
+           from all views
+        Observation:
+        I decided to use decorators for ease of command line printing and
+        the possibility of turn them off easily and fast by just commenting
+        the line to be able to reuse the real methods in another purpose.
+    '''
+
+    def __init__(self, dbname='news'):
+        self.dbname = dbname
+
+    @decorate_top3_articles
+    def get_top3_articles(self):
+        '''
+            Get top 3 most visited articles
+        '''
+        conn = psycopg2.connect(dbname=self.dbname)
+        cursor = conn.cursor()
+        query = '''
+                select a.title, concat(count(l.path), ' views')
+                from log l
+                inner join articles a on a.slug = substring(l.path, 10)
+                group by a.slug, a.title
+                order by count(l.path) desc
+                limit 3;
+            '''
+        cursor.execute(query)
+        result = cursor.fetchall()
+        conn.close()
+        return result
+
+    @decorate_top3_authors
+    def get_top3_authors(self):
+        '''
+            Get top 3 authors from top 3 articles
+        '''
+        conn = psycopg2.connect(dbname=self.dbname)
+        cursor = conn.cursor()
+        query = '''
+                select au.name, concat(count(l.path), ' views')
+                from log l
+                inner join articles a on a.slug = substring(l.path, 10)
+                inner join authors au on au.id = a.author
+                group by a.slug, au.name
+                order by count(l.path) desc
+                limit 3;
+            '''
+        cursor.execute(query)
+        result = cursor.fetchall()
+        conn.close()
+        return result
+
+    @decorate_percent1_error
+    def get_percent1_error(self):
+        '''
+            Get date where errors > 1% from all views
+        '''
+        conn = psycopg2.connect(dbname=self.dbname)
+        cursor = conn.cursor()
+        query = '''
+                create or replace view failure as
+                    select date(time) date, count(status)
+                    from log
+                    where status not like '200%'
+                    group by date(time)
+                    order by date(time);
+
+                create or replace view success as
+                    select date(time) date, count(status)
+                    from log
+                    where status like '200%'
+                    group by date(time)
+                    order by date(time);
+
+                select s.date,
+                    concat(round(f.count / s.count::numeric, 8) * 100, ' %')
+                from success s
+                inner join failure f
+                on f.date = s.date
+                where round(f.count / s.count::numeric, 8) * 100 > 1.0
+                order by s.date;
+            '''
+        cursor.execute(query)
+        result = cursor.fetchall()
+        conn.close()
+        return result
+
+
+if __name__ == '__main__':
+
+    report = Report()
+    report.get_top3_articles()
+    report.get_top3_authors()
+    report.get_percent1_error()
